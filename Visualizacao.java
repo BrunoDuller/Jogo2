76874e921e64de91d09b4518c680e20c868b4006
Dullinho_- <rm551942@fiap.com.br>
2023-05-11 13:24:46 -0300

    classe visualização

---
+++ Visualizacao.java
@@ -0,0 +1,15 @@
+package br.com.fiap.jogo2;
+
+public interface Visualizacao {
+	 
+	void setX(int x);
+	void setY(int y);
+	int getX();
+	int getY();
+
+	default void mover(int x, int y) {
+		setX(x);
+	    setY(y);
+	 }
+}
+
