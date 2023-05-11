b66d3dd1fb491bcb34661bfecac68eabaaca2596
Dullinho_- <rm551942@fiap.com.br>
2023-05-11 13:20:57 -0300

    classe fase

---
+++ Fase.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jogo2;
+
+import br.com.fiap.jogo2.Visualizacao;
+
+public class Fase {
+
+	public void carregar(Visualizacao objeto) {
+        System.out.println("Carregando objeto na posição:\nX:" + objeto.getX() + "\nY:" + objeto.getY());
+    }
+}
+	
+	
+
