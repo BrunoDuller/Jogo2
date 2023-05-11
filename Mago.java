7d2b68585745802851fa43d87a0d9d862e6d98e1
Dullinho_- <rm551942@fiap.com.br>
2023-05-11 13:23:25 -0300

    classe mago

---
+++ Mago.java
@@ -0,0 +1,67 @@
+package br.com.fiap.jogo2;
+
+public class Mago extends Jogador implements Visualizacao{
+
+	 private int x;
+	 private int y;
+	 private int mp;
+
+	 public Mago() {
+		 super();
+	     this.mp = 100;
+	     this.x = 0;
+	     this.y = 0;
+	 }
+	 
+	 public Mago(String nome) {
+		 super(nome);
+	     this.mp = 100;
+	     this.x = 0;
+	     this.y = 0;
+	    }
+	 public void ataqueMagico(Jogador jogador) {
+		 if (this.mp >= 10) {
+			 jogador.receberDano(this.getMp() * 2);
+	         this.mp -= 10;
+	        }
+	     if (jogador.getHp() <= 0) {
+	         this.ganharExperiencia(jogador.getXp());
+	         this.mp = 100;
+	     }
+	 }
+	 
+	 
+	 public void trocarVidaPorMana(int hp) {
+	     if (this.getHp() > hp) {
+	         this.receberDano(hp);
+	         this.mp += hp;
+	        }
+	    }
+	 
+	 
+	 public int getMp() {
+	     return mp;
+	 }
+
+	 public int getX() {
+		 return x;
+	 }
+
+	 public void setX(int x) {
+	     this.x = x;
+	 }
+	 
+	 public int getY() {
+	     return y;
+	 }
+
+	 public void setY(int y) {
+	     this.y = y;
+	 }
+
+}
+	 
+
+	 
+
+	    
