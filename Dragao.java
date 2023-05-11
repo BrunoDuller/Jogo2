c59fa5685bd8e498c3fd5ad4bdbaca0b2b70605d
Dullinho_- <rm551942@fiap.com.br>
2023-05-11 13:18:52 -0300

    Classe dragao

---
+++ Dragao.java
@@ -0,0 +1,30 @@
+package br.com.fiap.jogo2;
+
+public class Dragao implements Visualizacao {
+
+	private int x;
+    private int y;
+
+    public Dragao(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
+
